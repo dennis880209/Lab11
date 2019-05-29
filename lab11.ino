b58249135e1b854bbe874c8520606bcc2f81f967
dennis880209 <47806847+dennis880209@users.noreply.github.com>
2019-05-29 15:44:23 +0800

    Add files via upload

---
+++ lab11.ino
@@ -0,0 +1,77 @@
+#include <LWiFi.h>
+#include <WiFiClient.h>
+#include "MCS.h"
+
+#define _SSID "Yuan"
+#define _KEY  "12345678"
+
+MCSDevice mcs("DZwYqIhM", "x3SX3ZK7FkZDgDxB");
+
+MCSControllerOnOff led("123");
+MCSDisplayOnOff    remote("321");
+
+#define LED_PIN 7
+
+void setup() 
+{
+  Serial.begin(9600);
+
+  pinMode(LED_PIN, OUTPUT);
+
+  while(WL_CONNECTED != WiFi.status())
+  {
+    Serial.print("WiFi.begin(");
+    Serial.print(_SSID);
+    Serial.print(",");
+    Serial.print(_KEY);
+    Serial.println(")...");
+    WiFi.begin(_SSID, _KEY);
+  }
+  Serial.println("WiFi connected !!");
+
+  mcs.addChannel(led);
+  mcs.addChannel(remote);
+  while(!mcs.connected())
+  {
+    Serial.println("MCS.connect()...");
+    mcs.connect();
+  }
+  Serial.println("MCS connected !!");
+
+  while(!led.valid())
+  {
+    Serial.println("read LED value from MCS...");
+    led.value();
+  }
+  Serial.print("done, LED value = ");
+  Serial.println(led.value());
+  digitalWrite(LED_PIN, led.value() ? HIGH : LOW);
+}
+
+void loop() 
+{
+  Serial.print("process(");
+  Serial.print(millis());
+  Serial.println(")");
+  mcs.process(100);
+ 
+  if(led.updated())
+  {
+    Serial.print("LED updated, new value = ");
+    Serial.println(led.value());
+    digitalWrite(LED_PIN, led.value() ? HIGH : LOW);
+    if(!remote.set(led.value()))
+    {
+      Serial.print("Failed to update remote");
+      Serial.println(remote.value());
+    }
+  }
+ 
+  while(!mcs.connected())
+  {
+    Serial.println("re-connect to MCS...");
+    mcs.connect();
+    if(mcs.connected())
+      Serial.println("MCS connected !!");
+  }
+}
